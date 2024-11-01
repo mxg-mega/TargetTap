da998306635f836527cb8b1881fd9fa0920b7508
mxg-mega <68956501+mxg-mega@users.noreply.github.com>
2024-11-01 17:24:13 +0100

    Updates

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Target : MonoBehaviour, IPooledTarget
@@ -8,10 +9,19 @@ public class Target : MonoBehaviour, IPooledTarget
     public float elaspedTime = 0.0f;
     public float Duration { get; set; }
     public int ScoreValue { get; set; }
+    private Animator animator;
+    [SerializeField] private AnimationClip idle;
+    [SerializeField] private AnimationClip shrink;
+
 
     private void Awake()
     {
         ScoreValue = 1;
+        
+        if (gameObject.GetComponent<Animator>() != null)
+        {
+            animator = GetComponent<Animator>();
+        }
     }
     public virtual void OnSpawnTarget(int scoreValue = 1)
     {
@@ -25,16 +35,26 @@ public class Target : MonoBehaviour, IPooledTarget
         elaspedTime = 0.0f;
         Duration = 2.0f;
 
+        animator.Play(idle.name);
         // Wait for the target to be active for 'Duration' seconds
         yield return new WaitForSeconds(Duration);
 
+        // Play shrinking animation
+        StartCoroutine(PlayAnim(shrink));
         // Return the target to the object pool
         TargetPoolerManager.Instance.ReturnPooledTarget(gameObject);
     }
 
     public virtual void OnTapped()
     {
-        TargetPoolerManager.Instance.ReturnPooledTarget(gameObject);
         ScoreManager.Instance.AddScore(ScoreValue);
+        StartCoroutine(PlayAnim(shrink));
+        TargetPoolerManager.Instance.ReturnPooledTarget(gameObject);
+    }
+
+    private IEnumerator PlayAnim(AnimationClip clip)
+    {
+        animator.Play(clip.name);
+        yield return new WaitForSeconds(clip.length);
     }
 }
